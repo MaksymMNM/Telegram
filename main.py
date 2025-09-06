2b3782884308b98d153b33d9d46220ff42d9d19a
MaksymMNM <111454921+MaksymMNM@users.noreply.github.com>
2025-09-07 00:17:27 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import feedparser
+import telebot
+import time
+
+TOKEN = "1642655410:AAGMArNZH_WLlU9TBdNSgQCJ6CLCq-0QkgE"
+CHANNEL_ID = "@Ukrainian_Global_News"
+
+bot = telebot.TeleBot(TOKEN)
+
+# храним уже отправленные ссылки, чтобы не дублировать
+sent_links = set()
+
+def check_news():
+    feed = feedparser.parse("https://www.pravda.com.ua/rss/view_news/")
+    for entry in feed.entries:
+        if entry.link not in sent_links:
+            message = f"📰 {entry.title}\n{entry.link}"
+            bot.send_message(CHANNEL_ID, message)
+            sent_links.add(entry.link)
+
+if __name__ == "__main__":
+    while True:
+        check_news()
+        time.sleep(180)  # проверяем каждые 5 минут
